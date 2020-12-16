b0f6570f1238e55d607a6d94f499a330f5358f52
Ankit singh <ak94307@gmail.com>
2020-12-17 02:47:29 +0530

    comments and commeneted delete file code

---
+++ sample.py
@@ -0,0 +1,1 @@
+print("hellloooooooooooooooooooooo\n\n\nlooopp\n\nifff")
--- server.js
+++ server.js
@@ -10,27 +10,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/run', function (req, res) {
-   console.log("hello i am api ");
 
    const { exec } = require('child_process');
-
-   const runcode = (lan) => {
+    //function for running the code
+   const runcode = async (lan) => {
+       //dictionary for language to language command to run its code
        var lancmd = {
            python : ["python "],
            javascript : ["node "]
        };
+       //dictionary for file extension
        var ext = {
         python : ["py"],
         javascript : ["js"]
     };
+    //file name -- like-- sample.py
        fName = "sample."+ext[lan];
-
+    //writing code recived from body to a file 
        fs.writeFile(fName,req.body.code, (err) => {
            if(err)console.log(err);
            console.log("sucessfully written to the file");
        });
-
-       exec(lancmd[lan] + fName, (err, stdout, stderr) => {
+       //executing the code
+       await exec(lancmd[lan] + fName, (err, stdout, stderr) => {
            if (err) {
              console.error(`exec error: ${err}`);
              return;
@@ -40,16 +42,18 @@ app.get('/run', function (req, res) {
            res.send(stdout);
          });
 
-
-         fs.unlink(fName, (err) => {
-            if (err) throw err;
-            // if no error, file has been deleted successfully
-            console.log('File deleted!');
-        });
+        //deleting the file
+        //  fs.unlink(fName, (err) => {
+        //     if (err) throw err;
+        //     // if no error, file has been deleted successfully
+        //     console.log('File deleted!');
+        // });
    }
    let lan=req.body.lang ;
    runcode(lan);
 })
+
+
 
 const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
